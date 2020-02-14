9a88c924c54f2b186e466cd6aee35da3a94e22ba
SyedaMShafique <syedashafique@gmail.com>
2020-02-13 22:10:14 -0500

    Update GitPracticeClass.java

--- SyedaGitPracticeProject/src/GitPracticeClass.java
+++ SyedaGitPracticeProject/src/GitPracticeClass.java
@@ -4,7 +4,8 @@ public class GitPracticeClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Syeda Git Practice Java class");
-		System.out.println("COdeinHub comment");
+		System.out.println("CodeinHub comment");
+		System.out.println("Syeda Git Practice Java class");
 	}
 
 }
